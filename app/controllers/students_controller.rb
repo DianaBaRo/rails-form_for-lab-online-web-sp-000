1bb8375db26850d663bd30546412ee15f5ac89a7
Diana Badas <diana.badas@gmail.com>
2019-05-23 09:31:37 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,23 +13,23 @@ class StudentsController < ApplicationController
 
 	def create
 	  @student = Student.new(student_params(:first_name, :last_name))
-	  @post.save
-	  redirect_to post_path(@post)
+	  @student.save
+	  redirect_to student_path(@student)
 	end
 
 	def update
-	  @post = Post.find(params[:id])
-	  @post.update(post_params(:title))
-	  redirect_to post_path(@post)
+	  @student = Student.find(params[:id])
+	  @student.update(student_params(:first_name, :last_name))
+	  redirect_to student_path(@student)
 	end
 
 	def edit
-	  @post = Post.find(params[:id])
+	  @student = Student.find(params[:id])
 	end
 
 	private
 
-  def post_params(*args)
-    params.require(:post).permit(*args)
+  def student_params(*args)
+    params.require(:student).permit(*args)
   end
 end
